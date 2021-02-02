52d22a1ea4a18a8004791463db69817e782d5d50
Kurylo Alexey <kurylo1.20@gmail.com>
2021-02-02 19:53:29 +0300

    add date

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main (String[] args){
 		System.out.println("Hello cat");
 				System.out.println("Hello dog");
+				System.out.println("Hello mouse");
 	}
 }
